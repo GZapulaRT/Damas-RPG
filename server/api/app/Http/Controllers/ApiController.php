2cf997c283abf894aefb896490159a6f67ee4178
Mirtos <ifds9999@gmail.com>
2022-03-29 22:54:46 -0300

    DB REST API 29/03

--- server/api/app/Http/Controllers/ApiController.php
+++ server/api/app/Http/Controllers/ApiController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Player;
+use App\Models\Score;
+use App\Models\Status;
 
 class ApiController extends Controller
 {
@@ -21,21 +23,38 @@ class ApiController extends Controller
 
 
 	}
-	public function UpdateUser() {
+	public function UpdateUser(request $id) {
 		//Update Username and country in our user table
 
 	}
  
-	public function UpdateScoreData() {
+	public function UpdateScoreData(request $request) {
 		// Add a score record to our score table. Used to add all
 		// score records to give us the user rank placement
+		$score = new Score;
+		$score->user_id = $request->user_id;
+		$score->score_change = $request->score_change;
+		$score->save();
+		
+		return response()->json([
+			"message" => "essa bagaça funciona por Magica(\$seiLáOq)"
+		], 201);
 
 	}
 
-	public function UpdateStatus() {
+	public function UpdateStatus(request $request) {
 		// Add a status record in our status table. Used to have a record
 		// of penalizations of the user and the current state of their status.
 		// Also have comments in case clarification is needed
+		$status = new Status;
+		$status->user_id = $request->user_id;
+		$status->status_change = $request->status_change;
+		$status->status_comment = $request->status_comment;
+		$status->save();
+
+		return response()->json([
+			"message" => "essa bagaça funciona por Magica(\$seiLáOq)"
+		], 201);
 	}
 
 	public function GetAllUsers($place, $numberOfUsers) {
